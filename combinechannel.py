044801f146c4ef88439b5131b8fdce5f1ab9178c
lzqhkuubuntu <1431839116@qq.com>
2024-05-22 16:19:16 +0800

    change param

--- combinechannel.py
+++ combinechannel.py
@@ -2,8 +2,8 @@ import os
 import numpy as np
 from PIL import Image
 import cv2
-
-
+from segroom import Seg
+import json
 def getBaseNameWithoutExtension(path: str) -> str:
     return os.path.splitext(os.path.basename(path))[0]
 
@@ -25,17 +25,8 @@ def normalizeImage(img: np.ndarray,scale: float = 1) -> np.ndarray:
     img *= 255
     return img.astype(np.uint8)  # Convert img back to uint8
 
-path = "output_data"
-names = []
-for file in os.listdir(path):
-    
-    if file.endswith("_bbox.json"):
-        names.append(file.removesuffix("_bbox.json"))
-    
-set_names = set(names)
-names = list(set_names)
-for name in names:
-    
+
+def combinechannel(name):    
     ceiling_high = name+ "_ceiling_high_img.png"
     ceiling_low = name+ "_ceiling_low_img.png"
     floor_high = name+ "_floor_high_img.png"
@@ -49,14 +40,35 @@ for name in names:
     img1 = cv2.imread(path+"/"+ceiling_high, cv2.IMREAD_GRAYSCALE)
     img2 = cv2.imread(path+"/"+ceiling_low, cv2.IMREAD_GRAYSCALE)
     img3 = cv2.imread(path+"/"+floor_high, cv2.IMREAD_GRAYSCALE)
-
+    json_file_path = path+"/"+name+"_bbox.json"
+    with open(json_file_path, "r") as f:
+        data = json.load(f)
+    ceiling_high = data["ceiling"]["max"][2]
+    ceiling_low = data["ceiling"]["min"][2]
     #these magic numbers are the scale factors, come from chromatology theory
-    img1 = normalizeImage(img1,7) # A factor
-    img2 = normalizeImage(img2,7) # B factor
+    img1 = normalizeImage(img1,(ceiling_high-ceiling_low)/(ceiling_high-ceiling_low-3.15)) # A factor
+    img2 = normalizeImage(img2,(ceiling_high-ceiling_low)/(ceiling_high-ceiling_low-3.15)) # B factor
     img3 = normalizeImage(img3,0.1) # L factor
 
-    img2 = cv2.resize(img2, (img1.shape[1], img1.shape[0]))
-    img3 = cv2.resize(img3, (img1.shape[1], img1.shape[0]))
+    cv2.imwrite(path+"/temp/"+name+"_ceiling_high_adjustcolor.png", img1)
+    cv2.imwrite(path+"/temp/"+name+"_ceiling_low_adjustcolor.png", img2)
+    cv2.imwrite(path+"/temp/"+name+"_floor_high_adjustcolor.png", img3)
+    seg = Seg('model_weight/sam_vit_h_4b8939.pth')
+    mask1 = seg.segment(path+"/temp/"+name+"_ceiling_high_adjustcolor.png", path+"/"+name+"_ceiling_high_segmented.png",True)
+    mask2 = seg.segment(path+"/temp/"+name+"_ceiling_low_adjustcolor.png", path+"/"+name+"_ceiling_low_segmented.png",True)
+    mask3 = seg.segment(path+"/temp/"+name+"_floor_high_adjustcolor.png", path+"/"+name+"_floor_high_segmented.png")
+    print(len(mask1))
+    print(len(mask2))
+    print(len(mask3))
+    max_len = max(len(mask1),len(mask2),len(mask3))
+    min_len = min(len(mask1),len(mask2),len(mask3))
+    
+    img1 = (img1*(min_len/len(mask1))).astype(np.uint8)
+    img2 = (img2*(min_len/len(mask2))).astype(np.uint8)
+    img3 = (img3*(min_len/len(mask3))).astype(np.uint8)
+    
+    img1 = cv2.resize(img1, (img2.shape[1], img2.shape[0]))
+    img3 = cv2.resize(img3, (img2.shape[1], img2.shape[0]))
     
     
     rgb_img = cv2.merge((img3, img1, img2))
@@ -64,3 +76,18 @@ for name in names:
     cv2.imwrite(path+"/"+'{}_lab_image.png'.format(name), lab_img)
     cv2.imwrite(path+"/"+'{}_rgb_image.png'.format(name), rgb_img)
     
+
+path = "output_data"
+names = []
+for file in os.listdir(path):
+    
+    if file.endswith("_bbox.json"):
+        names.append(file.removesuffix("_bbox.json"))
+    
+set_names = set(names)
+names = list(set_names)
+list.sort(names)
+combinechannel("11_MedOffice_05_F4")
+for name in names:
+    combinechannel(name)
+
--- segroom.py
+++ segroom.py
@@ -2,37 +2,112 @@ from segment_anything import SamPredictor, sam_model_registry, SamAutomaticMaskG
 from matplotlib import pyplot as plt
 import numpy as np
 from PIL import Image
+import os
+from skimage.transform import rescale
+from skimage.filters.rank import minimum
+from skimage.morphology import disk, ball
+from skimage import img_as_ubyte
 
-sam = sam_model_registry["vit_h"](checkpoint="model_weight/sam_vit_h_4b8939.pth")
-sam.to(device='cuda')
-predictor = SamPredictor(sam)
-#using numpy to read image
+import numpy as np
 
+class Seg:
+    def __init__(self, model_path):
+        self.sam = sam_model_registry["vit_h"](checkpoint=model_path)
+        self.sam.to(device='cuda')
+        self.predictor = SamPredictor(self.sam)
+
+
+    def min_resize(self, image, new_size):
+        # Calculate the scale factors
+        y_scale = new_size[0] / image.shape[1]
+        x_scale = new_size[1] / image.shape[0]
+        image_uint8 = img_as_ubyte(image)
+        
+        filtered_image = np.empty_like(image_uint8)
+
+        if len(image_uint8.shape) == 2:
+    # The image is grayscale
+            filtered_image = minimum(image_uint8, disk(2))
+            rescaled = rescale(filtered_image, (x_scale,y_scale), mode='reflect')
+        else:
+            # The image is color
+            for channel in range(image_uint8.shape[2]):
+                filtered_image[:, :, channel] = minimum(image_uint8[:, :, channel], disk(2))
+            rescaled = rescale(filtered_image, (x_scale, y_scale,1), mode='reflect')
+        # Rescale the image
+
+        # Convert back to uint8
+        rescaled = (rescaled * 255).astype(np.uint8)
+
+        return rescaled
+    
+    def segment(self, read_path: str, save_path: str,min_resize=False)->list[dict[str, any]]:
+        img = Image.open(read_path)
+        # Resize the image so that the long side is 1024
+        # long_side = max(img.size)
+        # scale_factor = 2000 / long_side
+        # new_size = (round(img.size[0] * scale_factor), round(img.size[1] * scale_factor))
+        # if min_resize:
+        #     skimage_image = np.array(img)
+        #     skimage_image = self.min_resize(skimage_image, new_size)
+        #     img = Image.fromarray(skimage_image.astype(np.uint8))
+        # else:
+        #     img = img.resize(new_size, Image.BOX)
+        # plt.imsave('output_data/seg_room/resized.png', np.array(img))
+
+        img_rgb = img.convert('RGB')
+        # Convert the image to a numpy array
+        img_np = np.array(img_rgb)
+        max_size = max(img_np.shape[0],img_np.shape[1])
+        mask_generator: SamAutomaticMaskGenerator = SamAutomaticMaskGenerator(
+            model=self.sam,
+            points_per_side= max_size/60,# another magic number, but it works perfectly
+            points_per_batch= 32, #fit 16g vram perfectly
+            pred_iou_thresh= 0.92,
+            stability_score_thresh=0.75,
+            crop_n_layers=0,
+            crop_n_points_downscale_factor=1,
+            min_mask_region_area=50,  # Requires open-cv to run post-processing
+        )
+        masks = mask_generator.generate(img_np)
+        print(len(masks))
+        print(masks[0].keys())
+        plt.figure(figsize=(20,20))
+        plt.imshow(img_np*0.2)
+        show_anns(masks)
+        plt.axis('off')
+        plt.savefig(save_path)
+        plt.close()
+        print('Segmentation saved to', save_path)
+        return masks
+    
+  
+    
+
+    
+
+def show_anns(anns):
+    if len(anns) == 0:
+        return
+    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+    ax = plt.gca()
+    ax.set_autoscale_on(False)
+
+    img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
+    img[:,:,3] = 0
+    for ann in sorted_anns:
+        m = ann['segmentation']
+        color_mask = np.concatenate([np.random.random(3), [0.35]])
+        img[m] = color_mask
+    ax.imshow(img)
 
 # Read the image file
-img = Image.open('output_data/08_ShortOffice_01_F1_ceiling_low_img.png')
 
-# Resize the image so that the long side is 1024
-long_side = max(img.size)
-scale_factor = 1024 / long_side
-new_size = (round(img.size[0] * scale_factor), round(img.size[1] * scale_factor))
-img = img.resize(new_size)
-plt.imsave('output_data/seg_room/resized.png', np.array(img))
-
-img_rgb = img.convert('RGB')
-# Convert the image to a numpy array
-img_np = np.array(img_rgb)
-
-mask_generator: SamAutomaticMaskGenerator = SamAutomaticMaskGenerator(
-    model=sam,
-    points_per_side=32,
-    pred_iou_thresh=0.92,
-    stability_score_thresh=0.95,
-    crop_n_layers=0,
-    crop_n_points_downscale_factor=1,
-    min_mask_region_area=50,  # Requires open-cv to run post-processing
-)
-masks = mask_generator.generate(img_np)
-
-# Display the image
-#plt.imshow(masks)
+if __name__ == '__main__':
+    seg = Seg('model_weight/sam_vit_h_4b8939.pth')
+    path = 'output_data/'
+    for file in os.listdir(path):
+        if file.endswith('_lab_image.png') or file.endswith('_rgb_image.png'):
+            seg.segment(path + file, 'output_data/' + file + '_segmented.png')
+            
+#现对单个通道进行分解，统计分解数量，越少的权重越高
